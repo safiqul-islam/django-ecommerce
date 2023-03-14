18080ae3fa5e0457421957d19779ec62333946c1
Safiqul Islam <safiqulislam.green@gmail.com>
2023-03-15 01:24:25 +0600

    login and registration modal added

--- account/views.py
+++ account/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'frontend/home/home.html')
+
+
+def logfunction(request):
+    return render(request, 'frontend/auth/login.html')
